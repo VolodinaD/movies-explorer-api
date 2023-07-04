461120184e0856f4ab58a472519d80878cd6c250
Daria Volodina <dvol_1998@mail.ru>
2023-07-04 15:00:52 +0300

    добавить readme

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
 const cors = require('./middlewares/cors');
 
-const { PORT, DB_URL } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmdb' } = process.env;
 const app = express();
 
 mongoose.connect(DB_URL, {
